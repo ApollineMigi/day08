8e650f35606d6c622788dee0701a9b060146e00c
Apolline Miginiac <apollinemiginiac@MacBook-Pro-de-Apolline.local>
2021-07-07 15:29:06 +0200

    ex_05

---
+++ ex_05/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start ();
+if(isset ($SESSION["name"]));
+    $_SESSION["name"] = "platypus";
+if (isset($_GET['name'])){
+    $name = $_GET['name'];
+    if ($name)
+    $_SESSION["name"] = $name;
+}
+echo "Hello " . $_SESSION["name"];
+setcookie('name', $name);
+?>
