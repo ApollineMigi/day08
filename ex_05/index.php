f7263960ef26fc45d40ebf00fb7d3a7167b8df65
Apolline Miginiac <apollinemiginiac@MacBook-Pro-de-Apolline.local>
2021-07-07 16:51:06 +0200

    ex_05

--- ex_05/index.php
+++ ex_05/index.php
@@ -6,7 +6,7 @@ if (isset($_GET['name'])){
     $name = $_GET['name'];
     if ($name)
     $_SESSION["name"] = $name;
+    setcookie('name', $name);
 }
 echo "Hello " . $_SESSION["name"];
-setcookie('name', $name);
 ?>
